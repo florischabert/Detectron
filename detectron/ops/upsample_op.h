56fef823793f022720a00abe2ed60fb84c45de2b
Floris Chabert <fchabert@nvidia.com>
2018-07-09 16:32:05 -0400

    Fix build error in upsample

--- detectron/ops/upsample_op.h
+++ detectron/ops/upsample_op.h
@@ -35,19 +35,17 @@
    UpsampleOp(const OperatorDef& operator_def, Workspace* ws)
        : Operator<Context>(operator_def, ws), width_scale_(1), height_scale_(1) {
  
- if (HasArgument("width_scale")) {
-       width_scale_ = static_cast<T>(
-           OperatorBase::GetSingleArgument<float>("width_scale", 1));
-     }
-     if (HasArgument("height_scale")) {
-       height_scale_ = static_cast<T>(
-           OperatorBase::GetSingleArgument<float>("height_scale", 1));
-     }
-     CAFFE_ENFORCE_GT(width_scale_, 0);
-     CAFFE_ENFORCE_GT(height_scale_, 0);
- }
-   USE_OPERATOR_CONTEXT_FUNCTIONS;
- 
+        if (HasArgument("width_scale")) {
+            width_scale_ = static_cast<T>(
+                OperatorBase::GetSingleArgument<float>("width_scale", 1));
+            }
+        if (HasArgument("height_scale")) {
+            height_scale_ = static_cast<T>(
+                OperatorBase::GetSingleArgument<float>("height_scale", 1));
+            }
+        CAFFE_ENFORCE_GT(width_scale_, 0);
+        CAFFE_ENFORCE_GT(height_scale_, 0);
+    }
  
    bool RunOnDevice() override;
  
