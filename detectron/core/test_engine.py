94fe3902ce1459488b74e7bac909a8ddd39fafea
Floris Chabert <fchabert@nvidia.com>
2018-07-13 10:13:52 -0400

    Show dtype name

--- detectron/core/test_engine.py
+++ detectron/core/test_engine.py
@@ -349,9 +349,9 @@ def initialize_trt_engine(onnx_model, gpu_id=0):
     logger.info('Loading ONNX model...')
     model = onnx.load(onnx_model)
 
-    dtype = np.float16 if cfg.TEST.FP16 else np.float32
-    logger.info('Preparing TensorRT inference engine in {}...'.format(dtype))
-    engine = backend.prepare(model, device='CUDA:0', dtype=np.dtype)
+    dtype = np.dtype('float16') if cfg.TEST.FP16 else np.dtype('float32')
+    logger.info('Preparing TensorRT inference engine in {}...'.format(dtype.name))
+    engine = backend.prepare(model, device='CUDA:0', dtype=dtype)
     return engine
 
 
